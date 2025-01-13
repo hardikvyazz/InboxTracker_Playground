32708cea240b6d99079e81a305d6d842228d1450
hardikvyazz <hardikvyas.dev@gmail.com>
2025-01-13 19:24:23 +0530

    constants.ts added

---
+++ src/conf/constants.ts
@@ -0,0 +1,5 @@
+export const PORT = 3000;
+export const CREDENTIALS_PATH = '../../../credentials/gmail/credentials.json';
+export const TOKEN_PATH = '../../../credentials/gmail/';
+export const LAST_PROCESSED_FILE = '../../../credentials/gmail/last_processed.json';
+export const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
