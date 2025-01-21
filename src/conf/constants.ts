7cac07cfbbea5e500c7a30ee741012def53660a0
hardikvyazz <hardikvyas.dev@gmail.com>
2025-01-21 18:02:40 +0530

    conf has been changed

--- src/conf/constants.ts
+++ src/conf/constants.ts
@@ -1,8 +1,8 @@
 
 export const PORT = 3000;
-export const CREDENTIALS_PATH = '../../credentials/gmail/credentials.json';
-export const TOKEN_PATH = '../../credentials/gmail/token.json';
-export const LAST_PROCESSED_FILE = '../../credentials/gmail/last_processed.json';
+export const CREDENTIALS_PATH = '../credentials/gmail/credentials.json';
+export const TOKEN_PATH = '../credentials/gmail/token.json';
+export const LAST_PROCESSED_FILE = '../credentials/gmail/last_processed.json';
 export const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 import path from 'path';
 
