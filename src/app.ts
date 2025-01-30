912b3eb5767a8021ac09431de533c6f5ea378f1d
hardikvyazz <hardikvyas.dev@gmail.com>
2025-01-30 17:24:06 +0530

    app.js updated

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,31 @@
 import express from 'express';
 import { PORT } from './conf/constants';
-import { authorize } from './services/authService';
+import authUrlRouter from './services/authUrlService';  // Correct import of the router
+import refreshTokens from './services/tokenMonitorService';
 import { processReports } from './services/emailProcessor';
-import { log } from 'console';
 
 const app = express();
 
+// Attach the router to '/auth' endpoint
+app.use('/auth', authUrlRouter);
+
+app._router.stack.forEach((middleware: { route: { path: any; }; }) => {
+  if (middleware.route) {
+    console.log(middleware.route.path);
+  }
+});
+
+
 app.listen(PORT, async () => {
   console.log(`Server running on http://localhost:${PORT}`);
+
   try {
-    const auth = await authorize(app); // Pass the app instance
+    console.log('Starting token monitoring service...');
+    const auth = await refreshTokens(); // Start the background service for token refresh
     console.log('Authorized successfully.');
     
     await processReports(auth);
   } catch (error) {
-    console.error('Authorization Failed:', error);
+    console.error('Error in token monitoring:', error);
   }
 });
